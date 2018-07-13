c7c8e42f43ae05497e175607ba8c0735a6e79cea
Milo <420006482@qq.com>
2018-07-13 16:54:31 +0800

    测试

--- app.ext/src/main/java/com/milog/lombok/MyAnnotationProcessor.java
+++ app.ext/src/main/java/com/milog/lombok/MyAnnotationProcessor.java
@@ -50,13 +50,16 @@ public class MyAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
-
         Set<? extends Element> set = roundEnv.getElementsAnnotatedWith(MyGetter.class);
-        set.forEach(element -> {
+        messager.printMessage(Diagnostic.Kind.NOTE, "start " + set.size());
+
+        for(Element element : set) {
             JCTree jcTree = trees.getTree(element);
+            messager.printMessage(Diagnostic.Kind.NOTE, "tree " + jcTree.getClass().getCanonicalName());
             jcTree.accept(new TreeTranslator() {
                 @Override
                 public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
+                    messager.printMessage(Diagnostic.Kind.NOTE, "class " + jcClassDecl.toString());
                     List<JCTree.JCVariableDecl> jcVariableDeclList = List.nil();
 
                     for (JCTree tree : jcClassDecl.defs) {
@@ -65,16 +68,46 @@ public class MyAnnotationProcessor extends AbstractProcessor {
                             jcVariableDeclList = jcVariableDeclList.append(jcVariableDecl);
                         }
                     }
+                    messager.printMessage(Diagnostic.Kind.OTHER, "processing " + jcVariableDeclList.size());
 
-                    jcVariableDeclList.forEach(jcVariableDecl -> {
+                    for (JCTree.JCVariableDecl jcVariableDecl : jcVariableDeclList) {
                         messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.getName() + " has been processed");
                         jcClassDecl.defs = jcClassDecl.defs.prepend(makeGetterMethodDecl(jcVariableDecl));
-                    });
+                    }
                     super.visitClassDef(jcClassDecl);
                 }
 
+                @Override
+                public void visitAnnotation(JCTree.JCAnnotation jcAnnotation) {
+                    super.visitAnnotation(jcAnnotation);
+                    messager.printMessage(Diagnostic.Kind.NOTE, "annotation " + jcAnnotation.toString());
+                    messager.printMessage(Diagnostic.Kind.NOTE, "annotation " + jcAnnotation.getKind() + " tag" + jcAnnotation.getTag());
+                    messager.printMessage(Diagnostic.Kind.NOTE, "annotation " + jcAnnotation.args + " tag" + jcAnnotation.attribute.toString());
+                    for (JCTree.JCExpression expression : jcAnnotation.args) {
+                        messager.printMessage(Diagnostic.Kind.NOTE, "annotation " + jcAnnotation.args.get(0).getClass().getCanonicalName());
+                        if (expression instanceof JCTree.JCAssign) {
+                            JCTree.JCAssign assign = (JCTree.JCAssign) expression;
+                            JCTree.JCIdent ident = (JCTree.JCIdent) assign.lhs;
+                            ident.getName();
+
+                            JCTree.JCLiteral literal = (JCTree.JCLiteral) assign.rhs;
+                            literal.getValue();
+
+                            messager.printMessage(Diagnostic.Kind.NOTE, "annotation " + ident.getName());
+                            messager.printMessage(Diagnostic.Kind.NOTE, "annotation " + literal.getValue());
+                        }
+                    }
+                }
+
+                @Override
+                public void visitMethodDef(JCTree.JCMethodDecl jcMethodDecl) {
+                    super.visitMethodDef(jcMethodDecl);
+                    messager.printMessage(Diagnostic.Kind.NOTE, "method name" + jcMethodDecl.name);
+                    messager.printMessage(Diagnostic.Kind.NOTE, "method params" + jcMethodDecl.params);
+                }
             });
-        });
+        }
+
         return true;
     }
 
@@ -95,7 +128,7 @@ public class MyAnnotationProcessor extends AbstractProcessor {
         ListBuffer<JCTree.JCStatement> statements = new ListBuffer<>();
         statements.append(treeMaker.Return(treeMaker.Select(treeMaker.Ident(names.fromString("this")), jcVariableDecl.getName())));
         JCTree.JCBlock body = treeMaker.Block(0, statements.toList());
-        return treeMaker.MethodDef(treeMaker.Modifiers(Flags.PUBLIC), getNewMethodName(jcVariableDecl.getName()), jcVariableDecl.vartype, List.nil(), List.nil(), List.nil(), body, null);
+        return treeMaker.MethodDef(treeMaker.Modifiers(Flags.PUBLIC), getNewMethodName(jcVariableDecl.getName()), jcVariableDecl.vartype, null, null, null, body, null);
     }
 
     private Name getNewMethodName(Name name) {
