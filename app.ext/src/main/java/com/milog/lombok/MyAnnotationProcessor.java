01dcd78e6fa9bdd1b3e24e9b599d709572a03870
Milo <420006482@qq.com>
2018-07-18 20:40:08 +0800

    add options

--- app.ext/src/main/java/com/milog/lombok/MyAnnotationProcessor.java
+++ app.ext/src/main/java/com/milog/lombok/MyAnnotationProcessor.java
@@ -3,22 +3,7 @@ package com.milog.lombok;
 import com.milog.MyGetter;
 import com.milog.lombok.app.MyApp;
 import com.milog.lombok.javac.MiloProcessor;
-import com.sun.source.tree.Tree;
-import com.sun.tools.javac.api.JavacTrees;
-import com.sun.tools.javac.code.Flags;
-import com.sun.tools.javac.code.Symbol;
-import com.sun.tools.javac.code.Type;
-import com.sun.tools.javac.code.TypeTag;
-import com.sun.tools.javac.processing.JavacProcessingEnvironment;
-import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.TreeMaker;
-import com.sun.tools.javac.tree.TreeTranslator;
-import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.ListBuffer;
-import com.sun.tools.javac.util.Name;
-import com.sun.tools.javac.util.Names;
-
+import com.sun.tools.javac.main.JavaCompiler;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -27,8 +12,6 @@ import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 
@@ -39,6 +22,7 @@ import javax.tools.Diagnostic;
 
 public class MyAnnotationProcessor extends AbstractProcessor {
     private MiloProcessor processor;
+    private Messager messager;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -46,12 +30,13 @@ public class MyAnnotationProcessor extends AbstractProcessor {
         MyApp.init();
         processor = new MiloProcessor();
         processor.init(processingEnv);
+        messager = processingEnv.getMessager();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         processor.process(annotations, roundEnv);
-
+        messager.printMessage(Diagnostic.Kind.NOTE, "" + JavaCompiler.version());
 //        Set<? extends Element> set = roundEnv.getElementsAnnotatedWith(MyGetter.class);
 //        messager.printMessage(Diagnostic.Kind.NOTE, "start " + set.size());
 //
@@ -99,6 +84,5 @@ public class MyAnnotationProcessor extends AbstractProcessor {
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
     }
-
 
 }
--- app.ext/src/main/java/com/milog/lombok/javac/core/Javac8BasedLombokOptions.java
+++ app.ext/src/main/java/com/milog/lombok/javac/core/Javac8BasedLombokOptions.java
@@ -1,16 +1,16 @@
 /*
- * Copyright (C) 2009-2012 The Project Lombok Authors.
- *
+ * Copyright (C) 2013-2017 The Project Lombok Authors.
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,17 +19,27 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+package com.milog.lombok.javac.core;
 
-package com.milog.lombok.javac.handler;
+import com.sun.tools.javac.main.Option;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Options;
 
-import com.milog.MyGetter;
-
-/**
- * Created by miloway on 2018/7/17.
- */
-
-public class GetterHandler  extends JavacAnnotationHandler<MyGetter>{
-
-
-
+public class Javac8BasedLombokOptions extends LombokOptions {
+	public static Javac8BasedLombokOptions replaceWithDelombokOptions(Context context) {
+		Options options = Options.instance(context);
+		context.put(optionsKey, (Options) null);
+		Javac8BasedLombokOptions result = new Javac8BasedLombokOptions(context);
+		result.putAll(options);
+		return result;
+	}
+	
+	private Javac8BasedLombokOptions(Context context) {
+		super(context);
+	}
+	
+	@Override public void putJavacOption(String optionName, String value) {
+		String optionText = Option.valueOf(optionName).text;
+		put(optionText, value);
+	}
 }
